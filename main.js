90acdb3fa789671ee77b9d5fba238fea8de9118a
imAway <gelzen.crisanto@gmail.com>
2020-11-17 07:57:12 -0800

    separate pity + started daily reminder feature

--- main.js
+++ main.js
@@ -2,13 +2,35 @@
 const Discord = require('discord.js');
 const Wish = require('./wish-sim.js');
 const Token = require('./bottoken.js');
+const Daily = require('./daily-reminder.js');
 
 const client = new Discord.Client();
+
+let profiles = [];
 
 const prefix = '!';
 
 client.once('ready', () => {
     console.log('kekw in the chat!');
+    client.guilds.cache.array().forEach( async guild => {
+        await guild.members.fetch().then(members =>{
+            members.array().forEach(member => {
+                if(!member.user.bot){
+                    let person = {
+                        name: member.displayName,
+                        id: member.id,
+                        resins: 0,
+                        pity5L: 0,
+                        pity5R: 0,
+                        pity4R: 0,
+                        pity4L: 0
+                    }
+                    profiles.push(person);
+                }
+            })
+        })
+    })
+    setTimeout(() => {Wish.updateProfiles(profiles)}, 2000);
 });
 
 client.on("message", async msg => {
@@ -16,30 +38,27 @@ client.on("message", async msg => {
         let roll;
         switch(msg.content.substring(1).toLowerCase()){
             case "pull1r":
-                roll = Wish.regSingle();
+                roll = Wish.regSingle(msg.author.id);
                 msg.reply(`Here's what you got bitch! Name: ${roll.name} | Rating: ${roll.rating}* | Type: ${roll.type}`, { files: [`./${roll.type}s/${roll.src}`] });
                 break;
             case "pull1l":
-                roll = Wish.limSingle();
+                roll = Wish.limSingle(msg.author.id);
                 msg.reply(`Here's what you got bitch! Name: ${roll.name} | Rating: ${roll.rating}* | Type: ${roll.type}`, { files: [`./${roll.type}s/${roll.src}`] });
                 break;
-            case "pity4r":
-                msg.reply(`You are at ${Wish.pity4StarR()} pity for 4* on the regular banner`);
-                break;
             case "pity5r":
-                msg.reply(`You are at ${Wish.pity5StarR()} pity for 5* on the regular banner`);
-                break;
-            case "pity4l":
-                msg.reply(`You are at ${Wish.pity4StarL()} pity for 4* on the limited banner`);
+                msg.reply(`You are at ${Wish.pity5StarR(msg.author.id)} pity for 5* on the regular banner`);
                 break;
             case "pity5l":
-                msg.reply(`You are at ${Wish.pity5StarL()} pity for 5* on the limited banner`);
+                msg.reply(`You are at ${Wish.pity5StarL(msg.author.id)} pity for 5* on the limited banner`);
                 break;
             case "pull10r":
-                msg.reply("Here's what you got!", { files: [await Wish.regMulti()] })
+                msg.reply("Here's what you got!", { files: [await Wish.regMulti(msg.author.id)] })
                 break;
             case "pull10l":
-                msg.reply("Here's what you got!", { files: [await Wish.limMulti()] })
+                msg.reply("Here's what you got!", { files: [await Wish.limMulti(msg.author.id)] })
+                break;
+            case "testremind":
+                msg.reply(Daily.dailyReminder());
                 break;
         }
     }
--- wish-sim.js
+++ wish-sim.js
@@ -16,50 +16,42 @@ let threeStars;
 
 // Global variables to be used and reference in the function
 let urRoll;
-let multiRolls;
 
-let pity5StarR = 0;
-let pity4StarR = 0;
-let pity5StarL = 0;
-let pity4StarL = 0;
 let isLast4StarRateUp = true;
 let isLast5StarRateUp = true;
 
+let profiles = [];
 
-exports.limMulti = function(){
-    return combineAllPic(limitPull);
+exports.updateProfiles = function(profile){
+    profiles = profile;
 }
 
-exports.regMulti = function(){
-    return combineAllPic(regularPull);
+exports.limMulti = function(id){
+    return combineAllPic(limitPull, id);
 }
 
-exports.regSingle = function(){
-    return regularPull();
+exports.regMulti = function(id){
+    return combineAllPic(regularPull, id);
 }
 
-exports.limSingle = function(){
-    return limitPull();
+exports.regSingle = function(id){
+    return regularPull(id);
 }
 
-exports.pity4StarL = function(){
-    return pity4StarL;
+exports.limSingle = function(id){
+    return limitPull(id);
 }
 
-exports.pity4StarR = function(){
-    return pity4StarR;
+exports.pity5StarL = function(id){
+    return profiles.find(x => x.id == id).pity5L;
 }
 
-exports.pity5StarL = function(){
-    return pity5StarL;
-}
-
-exports.pity5StarR = function(){
-    return pity5StarR;
+exports.pity5StarR = function(id){
+    return profiles.find(x => x.id == id).pity5R;
 }
 
 // function that makes a single picture by combining all the picture you get from doing multi pulls
-async function combineAllPic(typeOfPull) {
+async function combineAllPic(typeOfPull, id) {
     // creates a canvas with the dimension
     let canvas = Canvas.createCanvas(1400, 560);
     let ctx = canvas.getContext('2d');
@@ -75,7 +67,7 @@ async function combineAllPic(typeOfPull) {
         }
 
         // variable to contain the object that was pulled based on the type of banner
-        urRoll = typeOfPull();
+        urRoll = typeOfPull(id);
         // draws the image of the object to the right location in the canvas
         ctx.drawImage(await Canvas.loadImage(`./${urRoll.type}s/${urRoll.src}`), x, y, 280, 280);
         x += 280;
@@ -85,25 +77,26 @@ async function combineAllPic(typeOfPull) {
 }
 
 // limited banner single pull implementation
-let limitPull = function limitedSinglePull() {
+let limitPull = function limitedSinglePull(id) {
     // sets up the three array
     updatePools(limitedBanner);
     // variable to contain the appropriate pool (3*, 4*, 5*)
     let pool;
     let isRateUpPull = false;
+    let puller = profiles.find(x => x.id == id);
 
     // checks the pity first, and assigns the correct array of objects based on conditions
-    if(pity5StarL == 89){
+    if(puller.pity5L == 89){
         pool = fiveStars;
-        pity5StarL = 0;
-        pity4StarL++;
-    } else if (pity4StarL == 9){
+        puller.pity5L = 0;
+        puller.pity4L++;
+    } else if (puller.pity4L == 9){
         pool = fourStars;
-        pity4StarL = 0;
-        pity5StarL++;
+        puller.pity4L = 0;
+        puller.pity5L++;
     } else{                             // runs if the pity hasn't been hit yet
         pool = bannerPull();
-        updatePity(pool, 'lim');
+        updatePity(pool,id, 'lim');
     }
 
     // checks if the the last character that was pulled is a rate up character on the banner
@@ -134,21 +127,21 @@ let limitPull = function limitedSinglePull() {
 }
 
 // pretty much the same implementation as the limited banner
-let regularPull = function regularSinglePull() {
+let regularPull = function regularSinglePull(id) {
     updatePools(regularBanner);
     let pool;
-
-    if(pity5StarR == 89){
+    let puller = profiles.find(x => x.id == id);
+    if(puller.pity5R == 89){
         pool = fiveStars;
-        pity5StarR = 0;
-        pity4StarR++;
-    } else if (pity4StarR == 9){
+        puller.pity5R = 0;
+        puller.pity4R++;
+    } else if (puller.pity4R == 9){
         pool = fourStars;
-        pity4StarR = 0;
-        pity5StarR++;
+        puller.pity4R = 0;
+        puller.pity5R++;
     } else{
         pool = bannerPull();
-        updatePity(pool, 'reg');
+        updatePity(pool, id, 'reg');
     }
     return ranObj = pool[Math.floor(Math.random() * pool.length)];
 }
@@ -168,33 +161,34 @@ function changePoolBasedOnRateUp(isRateUpPull, pool){
     }
 }
 
-function updatePity(pool, bannerType) {
+function updatePity(pool, id, bannerType) {
+    let puller = profiles.find(x => x.id == id);
     switch (pool[0].rating) {
         case 4:
             if(bannerType == 'lim'){
-                pity4StarL = 0;
-                pity5StarL++;
+                puller.pity4L = 0;
+                puller.pity5L++;
             } else {
-                pity4StarR = 0;
-                pity5StarR++;
+                puller.pity4R = 0;
+                puller.pity5R++;
             }
             break;
         case 5:
             if(bannerType == 'lim'){
-                pity4StarL++;
-                pity5StarL = 0;
+                puller.pity4L++;
+                puller.pity5L = 0;
             } else {
-                pity4StarR++;
-                pity5StarR = 0;
+                puller.pity4R++;
+                puller.pity5R = 0;
             }
             break;
         default:
             if(bannerType == 'lim'){
-                pity4StarL++;
-                pity5StarL++;
+                puller.pity4L++;
+                puller.pity5L++;
             } else {
-                pity4StarR++;
-                pity5StarR++;
+                puller.pity4R++;
+                puller.pity5R++;
             }
             break;
     }
